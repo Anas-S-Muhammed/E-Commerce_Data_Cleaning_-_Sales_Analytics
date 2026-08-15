f0a3f4dc7d6f9bb8d138f49eaa0b06169648344b
Anas-S-Muhammed <Ans455057@gmail.com>
2026-08-15 13:00:28 +0800

    feat: add raw table creation script with additional columns

---
+++ sql/01_create_raw_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE raw_sales_data (
+    id TEXT,
+    customer_name TEXT,
+    order_id TEXT,
+    order_date TEXT,
+    product TEXT,
+    category TEXT,
+    quantity TEXT,
+    price TEXT,
+    payment_method TEXT,
+    status TEXT,
+    total TEXT
+);
